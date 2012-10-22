740d429a485fc422e1f40a571693bb562a2ef795
Benjamin Lafaurie <benjamin.lafaurie@gmail.com>
2012-10-22 11:39:04 +0200

    Modifs Ben

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,10 +8,12 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
+        '192.168.1.98', //Benjamin
         '173.0.82.126', //Paypal
 		'87.91.25.164', //Test depuis l'ordi local mais en passant par internet (IP publique)
         '::1',
